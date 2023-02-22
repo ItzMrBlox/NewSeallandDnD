dbb7fc619d09243ce095bad91a14a002c80fd228
Blox <itzmrblox@gmail.com>
2023-02-22 20:14:37 +0100

    testasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
