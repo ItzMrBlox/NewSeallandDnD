a3eb992e630f4e0de35a1af8d6c8b6dcc6b77489
Blox <itzmrblox@gmail.com>
2023-02-24 19:52:18 +0100

    area move

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,13 @@ import { regions } from './data/regions.js';
 console.log(regions);
 
 document.querySelectorAll('area').forEach((area) => {
-    area.addEventListener('click', (e) => {
-        console.log(e)
+    area.addEventListener('mousemove', (e) => {
         e = e || window.event;
         var id = e.target.id;
 
         let region = regions.find(region => region.Id == id);
+
+        if(region == undefined) return;
 
         let infoBox = document.getElementsByClassName('infobox')[0];
         let infoName = document.getElementById('infobox_name');
@@ -18,6 +19,11 @@ document.querySelectorAll('area').forEach((area) => {
 
         infoBox.style.top = e.pageY + 'px';
         infoBox.style.left = e.pageX - (infoBox.offsetWidth / 2) + 'px';
+        infoBox.style.display = 'block';
 
     });
+});
+
+document.getElementById('whole_map').addEventListener('mousemove', (e) => {
+    document.getElementsByClassName('infobox')[0].style.display = 'none';
 });
